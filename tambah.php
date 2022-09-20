5b245d04a9a0a4e965539cd1b2a2c9aec6a4bc96
aleacitraramadhani <109120992+aleacitraramadhani@users.noreply.github.com>
2022-09-20 14:58:01 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>Form Tambah Barang</h1>
+        <form action="proses-tambah.php" method="POST">
+        <form>
+            <fieldset>
+                <p>
+                    <lable for="nama">Nama Barang :</label>
+                    <input type="text" name="nama" />
+                </p>
+                <p>
+                    <lable for="stok">Stok Barang :</label>
+                    <input type="number" name="nama" />
+                </p>
+                <p>
+                    <lable for="sharga">harga Barang :</label>
+                    <input type="text" name="nama" />
+                </p>
+                <p>
+                    <input type="submit" value="Tambah Barang" name="tambah" /> 
+                </p>
+           </fieldset>
+        </form>
+    </body>
+</html>
